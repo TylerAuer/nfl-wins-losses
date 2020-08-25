e59b15068ba1c2e984cfe21476e1b9fe32ac76f8
Tyler Auer <Tyler.Auer@gmail.com>
2020-08-25 16:33:10 -0700

    configure scripts for build and development

--- client/build/service-worker.js
+++ client/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.83447270db7b5fd91b68eb797b67dd39.js"
+  "/precache-manifest.86fe164748d27365b0eda2d1aa76283b.js"
 );
 
 self.addEventListener('message', (event) => {
