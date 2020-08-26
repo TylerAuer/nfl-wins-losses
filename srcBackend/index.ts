36a42b158ca892b5d0316e5f87d900bba14415f0
Tyler Auer <Tyler.Auer@gmail.com>
2020-08-26 08:28:36 -0700

    add structure for data routes in index.ts

--- srcBackend/index.ts
+++ srcBackend/index.ts
@@ -2,12 +2,31 @@ import express, { Request, Response } from 'express';
 const app = express();
 const port = process.env.PORT || 4000;
 
+////////////////////////////////////////////////////////////////////////////////
+//   SERVE CLIENT APP   ////////////////////////////////////////////////////////
+
 app.use(express.static(__dirname + '/client'));
 
-app.get('/', (req: Request, res: Response) =>
-  res.sendFile(__dirname + '/client/index.html')
-);
+app.get('/', (req: Request, res: Response): void => {
+  res.sendFile(__dirname + '/client/index.html');
+});
+
+////////////////////////////////////////////////////////////////////////////////
+//   BACKEND ROUTES   //////////////////////////////////////////////////////////
+
+app.get('/standings', (req: Request, res: Response): void => {
+  res.send('JSON for league standings');
+});
+app.get('/scoreboard', (req: Request, res: Response): void => {
+  res.send('JSON for live NFL scoreboard');
+});
+app.get('/bump', (req: Request, res: Response): void => {
+  res.send('Data for bump chart');
+});
+
+////////////////////////////////////////////////////////////////////////////////
+//   START SERVER   ////////////////////////////////////////////////////////////
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+  console.log(`Spinning up app on port: ${port}`);
 });
