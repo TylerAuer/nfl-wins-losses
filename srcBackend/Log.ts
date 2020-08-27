89844047c774966f0ef55baa8527f927e215468d
Tyler Auer <Tyler.Auer@gmail.com>
2020-08-26 22:11:33 -0700

    set up color-coded logging

---
+++ srcBackend/Log.ts
@@ -0,0 +1,15 @@
+import chalk from 'chalk';
+
+export class Log {
+  static cache(msg: string): void {
+    console.log(chalk.red(`CACHED: ${msg}`));
+  }
+
+  static send(msg: string): void {
+    console.log(chalk.blue(`SENT: ${msg}`));
+  }
+
+  static compute(msg: string): void {
+    console.log(chalk.green(`COMPUTED: ${msg}`));
+  }
+}
--- srcBackend/routes/scoreboard.ts
+++ srcBackend/routes/scoreboard.ts
@@ -1,22 +1,25 @@
 import { Request, Response } from 'express';
 import { cache } from '../cache';
+import { Log } from '../Log';
 import { Team } from '../classes/Team';
 import getLiveScores from '../callsToAPIs/getLiveScores';
 
 const sendScoreboard = async (req: Request, res: Response): Promise<void> => {
   if (cache.has('scoreboard')) {
-    console.log('From Cache');
     res.send(cache.get('scoreboard'));
+    Log.send('Scoreboard data');
   } else {
     // scoreboard data is not cached
     // so, generate it, send it, and cache it
     const teams: { [key: string]: Team } = req.app.locals.teams;
     const scoreboard = await getLiveScores(teams);
+    Log.compute('Scoreboard data');
 
-    console.log('Just calculated');
     res.send(scoreboard);
+    Log.send('Scoreboard data');
 
     cache.set('scoreboard', scoreboard, 10);
+    Log.cache('Scoreboard data');
   }
 };
 
