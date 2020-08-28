1f20a0053e9052e27c8ffad1383c433d7abe1e9e
Tyler Auer <Tyler.Auer@gmail.com>
2020-08-28 15:16:13 -0700

    make site title responsive

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -8,8 +8,8 @@ function App() {
   return (
     <div className="App">
       <Header />
-      <Scoreboard />
-      <Rankings />
+      {/* <Scoreboard />
+      <Rankings /> */}
     </div>
   );
 }
